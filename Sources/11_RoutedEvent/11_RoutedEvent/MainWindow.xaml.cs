cfd240bd4a865872cd91d65ddfe601902f0052ad
Kiranshankar Paul <mynokias60v3@gmail.com>
2017-11-22 23:01:59 +0530

    added routed event

---
+++ Sources/11_RoutedEvent/11_RoutedEvent/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _11_RoutedEvent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txt1.Text = "Button is Clicked";
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            txt2.Text = "Click event is bubbled to Stack Panel";
+            //use e.Handled to stop bubbling the event
+            e.Handled = true;
+        }
+
+        private void Window_Click(object sender, RoutedEventArgs e)
+        {
+            txt3.Text = "Click event is bubbled to Window";
+        }
+    }
+}
