7834857a02c191ecbbe072100f8ea6cac84a8d97
Kiranshankar Paul <mynokias60v3@gmail.com>
2017-11-24 11:40:03 +0530

    added converter ex

---
+++ Sources/22_ConverterEx/22_ConverterEx/Converters/BooleanToBrushConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace _22_ConverterEx.Converters
+{
+    public class BooleanToBrushConverter : IValueConverter
+    {
+        public Brush TrueBrush { get; set; }
+        public Brush FalseBrush { get; set; }
+        public Brush DefaultBrush { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                bool input = (bool)value;
+                if (input)
+                    return TrueBrush;
+                else
+                    return FalseBrush;
+            }
+            else
+                return DefaultBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
---
+++ Sources/22_ConverterEx/22_ConverterEx/Converters/FullNameConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace _22_ConverterEx.Converters
+{
+    public class FullNameConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values != null)
+            {
+                return string.Join(" ", values);
+            }
+            return " "; 
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            string[] values = null;
+            if (value != null)
+                return values = value.ToString().Split(' ');
+            return values;
+        }
+    }
+}
