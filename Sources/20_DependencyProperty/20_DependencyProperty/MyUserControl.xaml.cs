e972d8e14d1cb2e94d544045cf8cfa6a8b03e2da
Kiranshankar Paul <mynokias60v3@gmail.com>
2017-11-24 11:14:16 +0530

    added dependency property and attached event

---
+++ Sources/20_DependencyProperty/20_DependencyProperty/MyUserControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20_DependencyProperty
+{
+    /// <summary>
+    /// Interaction logic for MyUserControl.xaml
+    /// </summary>
+    public partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+        
+        public string CustomText
+        {
+            get { return (string)GetValue(CustomTextProperty); }
+            set { SetValue(CustomTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CustomText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CustomTextProperty =
+            DependencyProperty.Register("CustomText", typeof(string), typeof(MyUserControl), new PropertyMetadata("", new PropertyChangedCallback(OnSetTextChanged)));
+
+        private static void OnSetTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            MyUserControl UserControl1Control = d as MyUserControl;
+            UserControl1Control.OnSetTextChanged(e);
+        }
+
+        private void OnSetTextChanged(DependencyPropertyChangedEventArgs e)
+        {
+            lblSomeLabel.Text = e.NewValue.ToString();
+        }
+    }
+}
---
+++ Sources/21_AttachedProperty/21_AttachedProperty/TextBoxExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace _21_AttachedProperty
+{
+    public class TextBoxExtension
+    {
+        public static bool GetAllowOnlyString(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AllowOnlyStringProperty);
+        }
+        public static void SetAllowOnlyString(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AllowOnlyStringProperty, value);
+        }
+        // Using a DependencyProperty as the backing store for AllowOnlyString. This enables animation, styling, binding, etc...  
+        public static readonly DependencyProperty AllowOnlyStringProperty =
+        DependencyProperty.RegisterAttached("AllowOnlyString", typeof(bool), typeof(TextBoxExtension), new PropertyMetadata(false, AllowOnlyString));
+        private static void AllowOnlyString(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox)
+            {
+                TextBox txtObj = (TextBox)d;
+                txtObj.TextChanged += (s, arg) =>
+                {
+                    TextBox txt = s as TextBox;
+                    if (!Regex.IsMatch(txt.Text, "^[a-zA-Z]*$"))
+                    {
+                        txtObj.BorderBrush = Brushes.Red;
+                        MessageBox.Show("Only letter allowed!");
+
+                    }
+                };
+            }
+        }
+    }
+}
