11f996c8afe4bc22c697a28e1b404a01e2458dcf
Kiranshankar Paul <mynokias60v3@gmail.com>
2017-11-27 13:09:05 +0530

    added simple mvvm

---
+++ Sources/28_SimpleMVVM/28_SimpleMVVM/Converter/DatetimeToDateConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace _28_SimpleMVVM.Converter
+{
+    public class DatetimeToDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime && value != null)
+            {
+                DateTime date = (DateTime)value;
+                return date.ToString("MM/d/yyyy");
+            }
+            else
+                return "";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ Sources/28_SimpleMVVM/28_SimpleMVVM/ViewModel/UserViewModel.cs
@@ -0,0 +1,71 @@
+﻿using _28_SimpleMVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _28_SimpleMVVM.ViewModel
+{
+    public class UserViewModel : ViewModelBase
+    {
+        private User _user;
+        private ObservableCollection<User> _users;
+        private ICommand _SubmitCommand;
+        public User User
+        {
+            get
+            {
+                return _user;
+            }
+            set
+            {
+                _user = value;
+                NotifyPropertyChanged("User");
+            }
+        }
+        public ObservableCollection<User> Users
+        {
+            get
+            {
+                return _users;
+            }
+            set
+            {
+                _users = value;
+                NotifyPropertyChanged("Users");
+            }
+        }
+        public ICommand SubmitCommand
+        {
+            get
+            {
+                if (_SubmitCommand == null)
+                {
+                    _SubmitCommand = new RelayCommand(param => this.Submit(),
+                        null);
+                }
+                return _SubmitCommand;
+            }
+        }
+        public UserViewModel()
+        {
+            User = new User();
+            Users = new ObservableCollection<User>();
+            Users.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(Students_CollectionChanged);
+        }
+        //Whenever new item is added to the collection, am explicitly calling notify property changed
+        void Students_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            NotifyPropertyChanged("Users");
+        }
+        private void Submit()
+        {
+            User.EntryDate = DateTime.Today.Date;
+            Users.Add(User);
+            User = new User();
+        }
+    }
+}
