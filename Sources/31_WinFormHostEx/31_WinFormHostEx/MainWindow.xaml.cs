7b055793d4607259991a915598bcb190d666a60d
Kiranshankar Paul <mynokias60v3@gmail.com>
2017-11-28 13:14:12 +0530

    added new demo

---
+++ Sources/31_WinFormHostEx/31_WinFormHostEx/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _31_WinFormHostEx
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            (wfhSample.Child as System.Windows.Forms.WebBrowser).Navigate("http://www.google.com");
+        }
+
+        private void wbWinForms_DocumentTitleChanged(object sender, EventArgs e)
+        {
+            this.Title = (sender as System.Windows.Forms.WebBrowser).DocumentTitle;
+        }
+    }
+}
---
+++ Sources/32_LocalizationGlobalizationEx/32_LocalizationGlobalizationEx/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _32_LocalizationGlobalizationEx
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ru-RU");
+            //System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en"); 
+        }
+    }
+}
